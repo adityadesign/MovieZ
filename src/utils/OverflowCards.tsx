655c75bbd2dee2cbd450075cc23d822bdc47b39a
adityadesign <68054763+adityadesign@users.noreply.github.com>
2023-08-08 23:49:09 +0530

    removing lazy load

--- src/utils/OverflowCards.tsx
+++ src/utils/OverflowCards.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import type {Results, Movies} from '../features/movie-api-slice'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 interface OverflowCards {
     data: Movies
@@ -20,13 +19,8 @@ const OverflowCards: React.FC<OverflowCards> = ({data}) => {
             onClick={()=>[navigate(`/movie/${item.id}`), window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })]}
             className="flex text-sm justify-between flex-col relative flex-shrink-0 basis-32 ml-0 m-2 rounded-lg overflow-auto shadow-lg shadow-gray-950">
             {item.poster_path ? 
-              <LazyLoadImage
-                alt={item.title} height='11rem' width=''
-                src={`https://image.tmdb.org/t/p/w154${item.poster_path}`}/> : 
-              <LazyLoadImage
-                alt={'no poster'} height='11rem' width=''
-                src={'/no-poster.png'} />
-            }
+              <img className="max-h-44 object-cover" src={`https://image.tmdb.org/t/p/w154${item.poster_path}`}/> : 
+              <img className="max-h-44 object-cover" src='/no-poster.png' />}
             <div className="overflow-hidden text-ellipsis whitespace-nowrap mt-3 p-1">{item.title}{item.name}</div>
             <div className="flex justify-between">
               <div className="p-1 flex items-center" style={{fontSize: '0.75rem'}}>{dayjs(item.release_date).format("MMM D, YYYY")}</div>
